70aa44c3f976fb455f6636bbe75eed417f9dc7b0
Daniel Tavaszi <dtavaszi@hotmail.com>
2017-09-26 23:16:29 -0700

    Initial

---
+++ Assignment 1/Assignment1b.m
@@ -0,0 +1,47 @@
+% Approximating in increments of 2^k
+format long
+
+% Fig 2a
+k_max = 53;
+k = 0:1:k_max;
+
+x = power(2,k);
+y = power(1 + (1./x), x);
+
+x2 = 0;
+y2 = [exp(1), exp(1)];
+figure;plot(x, y);
+xlabel('N','fontsize',16);
+legend({'Approximation'},'fontsize',16,'Location','northeast');
+title('Fig. 2a - Underflow error as k approaches 53');
+
+% Fig 2b
+k_max = 51;
+k = 0:1:k_max;
+
+x = power(2,k);
+y = power(1 + (1./x), x);
+
+x2 = [0, power(2, k_max-1)];
+y2 = [exp(1), exp(1)];
+
+figure;plot(x, y, x2, y2);
+axis([0 power(10,15) 2.718281828459 2.7182818284591]);
+xlabel('N','fontsize',16);
+legend({'Approximation', 'Actual'},'fontsize',16,'Location','northwest');
+title('Fig. 2b - Approximation vs Actual');
+
+% Error plot
+
+actual = exp(1);
+absolute_error = abs(actual - y);
+relative_error = absolute_error / actual;
+
+figure;plot(x, absolute_error, x, relative_error);
+axis([0 power(2, k_max-1) 0 .8*power(10, -14)]);
+xlabel('N','fontsize',16);
+legend({'Absolute Error', 'Relative Error'},'fontsize',16,'Location','northeast');
+title('Fig. 2c - Approximation vs Actual Errors');
+
+
+
